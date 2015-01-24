56b5a8b79e3d7d6d4172b5baf9989f968bb224f5
Adrianf77 <adrianfracz@yahoo.co.uk>
2015-01-24 07:16:09 +0000

    Timer added

---
+++ Timer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	private static Timer s_timerInstance;
+
+	public static Timer Instance
+	{
+		get {return s_timerInstance;}
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		if(s_timerInstance == null)
+		{
+			s_timerInstance = this;
+		}
+	}
+
+	public void StartTimer(int time)
+	{
+		StartCoroutine(CountTime(time));
+	}
+
+	IEnumerator CountTime(int time)
+	{
+		float endTime = Time.time + time;
+
+		while(Time.time < endTime)
+		{
+			yield return null;
+		}
+
+		if(GameController.Instance.m_CurrentState == GameController.GAMESTATE.ACTIONSELECT)
+		{
+			GameController.Instance.SwitchState(GameController.GAMESTATE.ACTIONUSE);
+		}
+		else if(GameController.Instance.m_CurrentState == GameController.GAMESTATE.ACTIONUSE)
+		{
+			GameController.Instance.SwitchState(GameController.GAMESTATE.AITURN);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
