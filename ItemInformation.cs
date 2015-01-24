f5402066eaa5b32816836e11e97af9d1608eb8d2
davemccann <mccann28@googlemail.com>
2015-01-24 22:53:09 +0000

    Item Information script for items

---
+++ ItemInformation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemInformation : MonoBehaviour 
+{
+    public enum ItemType { Defence, SafeCode, Evidence, CarKeys, Fuel, BathroomKey, BedroomKey, DoorKey, GarageKey, HouseKey, LibraryKey, StudyKey };
+
+    public ItemType itemType;
+
+    public ItemLookup.Item item;
+
+    public bool beenUsed { get; set; }
+
+    public void Start()
+    {
+        beenUsed = false;
+    }
+
+    public void FixedUpdate()
+    {
+        if (beenUsed && item.m_OneUse)
+            Destroy(this);
+    }
+}
