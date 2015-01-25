46e930439058cfd9654973872adac2b34987f051
davemccann <mccann28@googlemail.com>
2015-01-25 13:56:04 +0000

    Twat2

--- Objects/Bath.cs
+++ Objects/Bath.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
-
-public class Bath : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Bath : ObjectClass {
 	
+	void Start () {
+		m_PossibleActions.Add (ActionController.ACTIONS.EXAMINE);
+		m_PossibleActions.Add (ActionController.ACTIONS.SAFECRACK);
+		m_PossibleActions.Add (ActionController.ACTIONS.LOCKPICK);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public override void Interact (ActionController.ACTIONS action) 
+	{
+		switch(action)
+		{
+		case ActionController.ACTIONS.EXAMINE:
+			GameController.Instance.FireDialogue("A very large bath. Two people\n would fit easily in this.");
+			break;
+			
+			//if i find an item something new hey yoo la gorgeous bastard 101!!!!!!!
+			
+		case ActionController.ACTIONS.SAFECRACK:
+			GameController.Instance.FireDialogue("A bath disguised as a safe!?\n Maybe one day but not this day.");
+			break;
+		case ActionController.ACTIONS.LOCKPICK:
+			GameController.Instance.FireDialogue("Where's the keyhole? Oh wait... \n There isn't one.");
+			
+			break;
+		}
 	}
 }
