86abc30ced30649074a4b46aae729117755bd032
davemccann <mccann28@googlemail.com>
2015-01-25 13:50:31 +0000

    Twat

---
+++ Objects/Fridge.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine;
+using System.Collections;
+public class Fridge : ObjectClass {
+	
+	void Start () {
+		m_PossibleActions.Add (ActionController.ACTIONS.EXAMINE);
+		m_PossibleActions.Add (ActionController.ACTIONS.SAFECRACK);
+		m_PossibleActions.Add (ActionController.ACTIONS.LOCKPICK);
+	}
+	
+	public override void Interact (ActionController.ACTIONS action) 
+	{
+		switch(action)
+		{
+		case ActionController.ACTIONS.EXAMINE:
+			GameController.Instance.FireDialogue("A big fridge with all sorts of stuff inside!");
+			break;
+			
+			//if i find an item something new hey yoo la gorgeous bastard 101!!!!!!!
+			
+		case ActionController.ACTIONS.SAFECRACK:
+			GameController.Instance.FireDialogue("Sadly this is not a safe.");
+			break;
+		case ActionController.ACTIONS.LOCKPICK:
+			GameController.Instance.FireDialogue("Lockpick a fridge? \nI don't want that future.");
+			
+			break;
+		}
+	}
+}
+
